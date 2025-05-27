8436a2dee821352e2353c0054695f2e7e6e5d206
eojin <chch6879@naver.com>
2025-05-27 16:36:48 +0900

    new auto slide 분야별로

--- newspectrum/src/main/java/com/capstone/newspectrum/controller/MainController.java
+++ newspectrum/src/main/java/com/capstone/newspectrum/controller/MainController.java
@@ -2,12 +2,8 @@ package com.capstone.newspectrum.controller;
 
 import com.capstone.newspectrum.dto.*;
 import com.capstone.newspectrum.enumeration.Domain;
-import com.capstone.newspectrum.enumeration.Media;
 import com.capstone.newspectrum.service.MainPageSevice;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.capstone.newspectrum.service.SectionPageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,12 +16,15 @@ import java.util.*;
 public class MainController {
     @Autowired
     private MainPageSevice mainPageService;
+    @Autowired
+    private SectionPageService sectionPageService;
 
     @GetMapping("/")
     public String main(Model model) {
         System.out.println("테스트 실행 Java 버전: " + System.getProperty("java.version"));
 
         LocalDateTime today = LocalDateTime.of(2025, 2, 28, 23, 59);
+        LocalDateTime start = today.minusDays(1);
 
         Map<String, MainBlockDTO> issuesByDomain = mainPageService.get_main_block_list(today);
 
@@ -50,6 +49,19 @@ public class MainController {
 //        });
 
         model.addAttribute("issuesByDomainJson", issuesByDomain);
+
+        // 각 도메인별로 하나씩 대표 이슈 추출
+        List<IssueDTO> hotIssues = new ArrayList<>();
+        for (Domain domain : Domain.values()) {
+            List<IssueDTO> domainIssues = sectionPageService.get_issue_by_domain(start, today, domain);
+            if (!domainIssues.isEmpty()) {
+                hotIssues.add(domainIssues.get(0));  // 가장 큰 클러스터 하나만 사용
+                System.out.println("🔥 Domain: " + domain.name());
+            }
+        }
+
+        model.addAttribute("hotIssues", hotIssues);
+
         return "main";
     }
 
--- newspectrum/src/test/java/com/capstone/newspectrum/MainPageServiceTest.java
+++ newspectrum/src/test/java/com/capstone/newspectrum/MainPageServiceTest.java
@@ -7,8 +7,10 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.AbstractEnvironment;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
