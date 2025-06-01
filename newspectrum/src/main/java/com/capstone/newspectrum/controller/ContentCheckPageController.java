b05818d4f7502c542bb23caa83fc2d3562f6949a
eojin <chch6879@naver.com>
2025-06-01 16:32:52 +0900

    controller 수정

--- newspectrum/src/main/java/com/capstone/newspectrum/controller/ContentCheckPageController.java
+++ newspectrum/src/main/java/com/capstone/newspectrum/controller/ContentCheckPageController.java
@@ -11,21 +11,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/contentCheck")
 public class ContentCheckPageController {
     @Autowired
     NewsArticleService newsArticleService;
-    @GetMapping("/{news_Article_id}")
-    public String contentCheck(Model model,
-                               @PathVariable("news_article_id") Long news_article_id){
-    NewsArticleDTO news_article = newsArticleService.get_news_article_by_id(news_article_id);
-    List<ContentCheckDTO> news_article_content_check = news_article.getContentChecks();
 
-    model.addAttribute("news_article", news_article);
-    model.addAttribute("content_check", news_article_content_check);
-    return "news_article_contentCheck";
+    @GetMapping("/contentcheck/{news_article_id}")
+    public String contentCheck(Model model,
+                               @PathVariable("news_article_id") Long news_article_id) {
+        NewsArticleDTO news_article = newsArticleService.get_news_article_by_id(news_article_id);
+
+        if (news_article == null) {
+            System.out.println("❌ 뉴스 기사 없음. ID = " + news_article_id);
+            return "error/404"; // 또는 템플릿 만들어도 OK
+        }
+
+//        List<ContentCheckDTO> news_article_content_check = news_article.getContentChecks();
+        List<ContentCheckDTO> news_article_content_check = new ArrayList<>();
+
+//        if (news_article_content_check == null)
+//            news_article_content_check = new ArrayList<>();
+
+        model.addAttribute("news_article", news_article);
+        model.addAttribute("content_check", news_article_content_check);
+
+        return "contentcheck";
     }
 }
