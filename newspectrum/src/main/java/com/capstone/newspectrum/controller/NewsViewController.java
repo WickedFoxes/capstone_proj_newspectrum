a25534824a3b11a52b3358e62772075c8fc232e0
eojin <chch6879@naver.com>
2025-06-05 14:51:48 +0900

    수정 없음

--- newspectrum/src/main/java/com/capstone/newspectrum/controller/NewsViewController.java
+++ newspectrum/src/main/java/com/capstone/newspectrum/controller/NewsViewController.java
@@ -28,6 +28,7 @@ public class NewsViewController {
         List<KeywordDTO> keyword_items = newsArticleService.get_keyword_items_by_id(news_article_id);
         List<RelatedNewsArticleAndScoreDTO> related_news_articles = newsArticleService.get_related_news_articles_by_id(news_article_id);
 
+
         // AI 요약을 마침표로 분할
         List<String> summaryLines = new ArrayList<>();
         if (news_article.getSummary() != null) {
@@ -37,7 +38,6 @@ public class NewsViewController {
                 if (summaryLines.size() == 3) break;
             }
         }
-
 
         model.addAttribute("news_article", news_article);
         model.addAttribute("summaryLines", summaryLines);
